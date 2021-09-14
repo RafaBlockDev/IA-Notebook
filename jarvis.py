0e0006a3cb1b42a60c04073b57f33b4e430daa8e
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-13 20:33:48 -0500

    Adding voices

--- jarvis.py
+++ jarvis.py
@@ -1,6 +1,15 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+
+engine.say("Buenos días Rafael")
+engine.runAndWait()
 
 try:
     with sr.Microphone() as source:
