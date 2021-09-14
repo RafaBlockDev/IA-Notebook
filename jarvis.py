7b9ba472974b8ebd1828125633f6d4e46a022e0d
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-13 21:17:55 -0500

    Adding pywhatkit

--- jarvis.py
+++ jarvis.py
@@ -1,27 +1,37 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
 
+name = "jarvis"
 listener = sr.Recognizer()
 
 engine = pyttsx3.init()
 
-
-try:
-    with sr.Microphone() as source:
-        print("Escuchando...")
-        voice = listener.listen(source)
-        rec = listener.recognize_google(voice)
-        print(rec)
-        
-except:
-    pass
-
-
 voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[1].id)
+engine.setProperty("voice", voices[0].id)
 
-engine.say("Buenas noches Rafael")
-engine.runAndWait()
+def listen():
 
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            voice = listener.listen(source)
+            rec = listener.recognize_google(voice)
+            rec = rec.lower()
+            if name in rec:
+                rec = rec.replace(name, "")
+                print(rec)
+        
+    except:
+        pass
+    return rec
 
+def run():
+    rec = listen()
+    if "reproduce" in rec:
+        music = rec.replace("reproduce", "")
+        talk("Reproduciendo" + music)
+        pywhatkit.playonyt(music)
+
+run()
 
